4594598d73c7c48c2d03e7e70c8d4ab65a585f73
EvoL_ <EvoL_@MOON>
2024-01-14 20:51:44 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("1. Чем является Яблоко?");
+            //Console.WriteLine("a) Овощь");
+            //Console.WriteLine("b) Фрукт");
+            //Console.WriteLine("c) Трава");
+
+            //char correctAnswer = 'b';
+
+            //Console.Write("Введите ваш ответ (a, b или c): ");
+            //char userAnswer = char.ToLower(Console.ReadKey().KeyChar);
+
+            //Console.WriteLine();
+
+            //if (userAnswer == correctAnswer)
+            //{
+            //    Console.WriteLine("Верно! Поздравляю!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"Неправильно. Правильный ответ: {correctAnswer}");
+
+            //Console.Write("Введите продолжительность разговора в минутах: ");
+            //double duration = Convert.ToDouble(Console.ReadLine());
+
+            //Console.Write("Введите день недели (1-7): ");
+            //int dayOfWeek = Convert.ToInt32(Console.ReadLine());
+
+            //double costPerMinute = 1.0;
+            //double discount = 0.2;
+
+            //double totalCost = duration * costPerMinute;
+
+            //if (dayOfWeek == 6 || dayOfWeek == 7)
+            //{
+            //    totalCost -= totalCost * discount;
+            //}
+
+            //Console.WriteLine("Стоимость переговоров: " + totalCost);
+
+            //Console.WriteLine("Введите текущее время (часы от 0 до 23): ");
+            //int currentHour = Convert.ToInt32(Console.ReadLine());
+
+            //string timeOfDay;
+
+            //if (currentHour >= 6 && currentHour < 12)
+            //{
+            //    timeOfDay = "утро";
+            //}
+            //else if (currentHour >= 12 && currentHour < 18)
+            //{
+            //    timeOfDay = "день";
+            //}
+            //else if (currentHour >= 18 && currentHour < 24)
+            //{
+            //    timeOfDay = "вечер";
+            //}
+            //else
+            //{
+            //    timeOfDay = "ночь";
+            //}
+
+            //Console.WriteLine($"Сейчас {timeOfDay}.");
+
+
+            //Console.WriteLine("Введите средний балл по диплому (от 3 до 5): ");
+            //double averageGrade = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("Введите стаж работы по специальности (в годах): ");
+            //int workExperience = Convert.ToInt32(Console.ReadLine());
+
+            //double coefficient;
+
+            //if (workExperience < 2)
+            //{
+            //    coefficient = 13;
+            //}
+            //else if (workExperience >= 2 && workExperience <= 5)
+            //{
+            //    coefficient = 16;
+            //}
+            //else
+            //{
+            //    coefficient = 1;
+            //}
+            //double rating = averageGrade * coefficient;
+
+            //Console.WriteLine($"Рейтинг бакалавра: {rating}");
+
+            //if (rating >= 45)
+            //{
+            //    Console.WriteLine("Поздравляем! Вы приняты в магистратуру.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("К сожалению, ваш рейтинг не достаточен для поступления в магистратуру.");
+            //}
+
+            //Console.Write("Введите день: ");
+            //int day = Convert.ToInt32(Console.ReadLine());
+
+            //Console.Write("Введите месяц: ");
+            //int month = Convert.ToInt32(Console.ReadLine());
+
+            //string zodiacSign = "";
+
+            //if ((month == 3 && day >= 22) || (month == 4 && day <= 21))
+            //{
+            //    zodiacSign = "Овен";
+            //}
+            //else if ((month == 4 && day >= 22) || (month == 5 && day <= 21))
+            //{
+            //    zodiacSign = "Телец";
+            //}
+            //else if ((month == 5 && day >= 22) || (month == 6 && day <= 21))
+            //{
+            //    zodiacSign = "Близнецы";
+            //}
+            //else if ((month == 6 && day >= 22) || (month == 7 && day <= 21))
+            //{
+            //    zodiacSign = "Рак";
+            //}
+            //else if ((month == 7 && day >= 22) || (month == 8 && day <= 21))
+            //{
+            //    zodiacSign = "Лев";
+            //}
+            //else if ((month == 8 && day >= 22) || (month == 9 && day <= 21))
+            //{
+            //    zodiacSign = "Дева";
+            //}
+            //else if ((month == 9 && day >= 22) || (month == 10 && day <= 21))
+            //{
+            //    zodiacSign = "Весы";
+            //}
+            //else if ((month == 10 && day >= 22) || (month == 11 && day <= 21))
+            //{
+            //    zodiacSign = "Скорпион";
+            //}
+            //else if ((month == 11 && day >= 22) || (month == 12 && day <= 21))
+            //{
+            //    zodiacSign = "Стрелец";
+            //}
+            //else if ((month == 12 && day >= 22) || (month == 1 && day <= 21))
+            //{
+            //    zodiacSign = "Козерог";
+            //}
+            //else if ((month == 1 && day >= 22) || (month == 2 && day <= 21))
+            //{
+            //    zodiacSign = "Водолей";
+            //}
+            //else if ((month == 2 && day >= 22) || (month == 3 && day <= 21))
+            //{
+            //    zodiacSign = "Рыбы";
+            //}
+            //Console.WriteLine("Ваш знак Зодиака: " + zodiacSign);
+
+            //Console.Write("Введите число от 1 до 99: ");
+            //int number = Convert.ToInt32(Console.ReadLine());
+
+            //string word = "";
+
+            //if (number % 10 == 1 && number % 100 != 11)
+            //    word = "копейка";
+            //else if (number % 10 >= 2 && number % 10 <= 4 && (number % 100 < 10 || number % 100 >= 20))
+            //    word = "копейки";
+            //else
+            //    word = "копеек";
+
+            //Console.WriteLine(number + " " + word);
+
+
+            //Console.Write("Введите число от 1 до 99: ");
+            //int number = Convert.ToInt32(Console.ReadLine());
+
+            //string word = "";
+
+            //if (number % 10 == 1 && number % 100 != 11)
+            //    word = "рубль";
+            //else if (number % 10 >= 2 && number % 10 <= 4 && (number % 100 < 10 || number % 100 >= 20))
+            //    word = "рубля";
+            //else
+            //    word = "рублей";
+
+            //Console.WriteLine(number + " " + word);
+
+            //Console.ReadKey();
+        }
+    }
+}  
+              
+        
+            
+    
+   
+
+    
+
